d1f2ccc3631d54a28e83b71845079057f1809381
Liam <liam-letts@hotmail.com>
2023-10-12 00:55:27 +0200

    Added Rotation class

---
+++ Rotation.java
@@ -0,0 +1,36 @@
+public class Rotation {
+    public static void main(String[] args){
+        int[] nums = {8,9,10,1,2,3,4,5,6,7};
+        System.out.println(binary_search(nums));
+    }
+
+    public static int binary_search(int[] nums) {
+        int left = 0;
+        int right = nums.length-1;
+        int mid = 0;
+
+        while(left<=right){
+            if(nums[left] <= nums[right]){
+                return left;
+            }
+            mid = (left+right)/2;
+
+            int prev = (mid-1)%nums.length;
+            int next = (mid+1)%nums.length;
+
+            if(nums[mid] <= nums[prev] && nums[mid] <= nums[next] ){
+                return mid;
+            }
+
+            else if(nums[mid] <= nums[right]){
+                right = mid-1;
+            }
+            else if(nums[mid] >= nums[left]){
+                left = mid +1;
+            }
+        }
+        return -1;
+    }
+
+
+}
